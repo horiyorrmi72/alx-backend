f0480299a66068a401d6a523ea3dcd6733f2b045
yusuf ridwan <horiyorrmi72@gmail.com>
2024-12-08 01:25:03 +0000

    basic caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class
+    """
+    def __init__(self):
+        """
+        initializing the class
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key.
+        If key or item is None, this method should not do anything
+        """
+        if key and item:
+            self.cache_data[key] = item
+        else:
+            pass
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data,
+        return None
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
