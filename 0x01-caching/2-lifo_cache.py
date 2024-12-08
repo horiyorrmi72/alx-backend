1ca7411db5da2e9bf186dc1bb1236207583bc294
yusuf ridwan <horiyorrmi72@gmail.com>
2024-12-08 02:12:26 +0000

    last in first out

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,7 +20,8 @@ class LIFOCache(BaseCaching):
         """
         Assigning key and item
         if key or item is None, this method should not do anything.
-        If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
+        If the number of items in self.cache_data is higher
+        that BaseCaching.MAX_ITEMS:
         you must discard the last item put in cache (LIFO algorithm)
         """
         if len(self.cache_data) == self.MAX_ITEMS and key not in self.stack:
@@ -35,7 +36,8 @@ class LIFOCache(BaseCaching):
         """
         Fetching  data with key
         Must return the value in self.cache_data linked to key.
-        If key is None or if the key doesn’t exist in self.cache_data, return None
+        If key is None or if the key doesn’t exist in self.cache_data,
+        return None
         """
         if not key or key not in self.cache_data.keys():
             return None
